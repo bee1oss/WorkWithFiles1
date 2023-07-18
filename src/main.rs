637a21f2b13b04f2c4800f26b05cee90c20ab1a3
bee1oss <46090840+bee1oss@users.noreply.github.com>
2023-07-18 12:22:14 +0300

    a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::{Read};
+
 fn main() {
-    println!("Hello, world!");
+    //File::create("text.txt").expect("Error creating file!!");create sadece file olusturma degil only-write yani sadece icine bir seyler yazabiliyoruz
+    //File::open("text.txt").expect("Error creating file!!");open ise bir dosyayi acmak ve sadece okumak icin
+
+    let path = "text.txt";
+
+    let mut f = File::open(path).expect("Error opening file!");
+
+    //f.write_all("Hello from bega".as_bytes()).expect("Error writing to file!");
+
+    let mut file_text = String::new();
+
+    f.read_to_string(&mut file_text).expect("Error reading file!");
+
+    println!("{}",file_text);
+
 }
